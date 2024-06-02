9a30684ab5714c5067dc946fee34dff7fa563bbc
shmil123 <arielkeren2007@gmail.com>
2024-06-02 14:57:03 +0300

    Add comments to setUtils.h

--- setUtils.h
+++ setUtils.h
@@ -3,7 +3,22 @@
 
 #include "globals.h"
 
+/**
+ * Checks if the given set is empty.
+ *
+ * @param setA The set to check.
+ * @return TRUE if the set is empty, FALSE otherwise.
+ */
 boolean isSetEmpty(const set setA);
+
+/**
+ * Converts a non-empty set to its string representation and prints it.
+ * Assumes the set is non-empty.
+ * Every number is the set is separated by a comma and a space.
+ * No more than 16 numbers shall occupy the same line.
+ *
+ * @param setA The set to print.
+ */
 void printSetAsString(const set setA);
 
 #endif
